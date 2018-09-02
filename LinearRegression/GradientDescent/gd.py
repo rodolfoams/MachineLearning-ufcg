869417827567c545113f395ff49e70db5e3cac64
Rodolfo Marinho <rodolfo@grupomarinho.com.br>
2018-09-02 09:46:36 -0300

    Organizing files

--- LinearRegression/GradientDescent/gd.py
+++ LinearRegression/GradientDescent/gd.py
@@ -1,9 +1,9 @@
-from numpy import *
+from numpy import array, genfromtxt
 import argparse
 
 def compute_error_for_line_given_points(b, m, points):
     totalError = 0
-    for i in xrange(len(points)):
+    for i in range(len(points)):
         x = points[i, 0]
         y = points[i, 1]
         totalError += (y - (m*x + b)) ** 2
@@ -15,7 +15,7 @@ def step_gradient(b_current, m_current, points, learning_rate):
     b_gradient = 0
     m_gradient = 0
     N = float((len(points)))
-    for i in xrange(int(N)):
+    for i in range(int(N)):
         x = points[i, 0]
         y = points[i, 1]
         b_gradient += -(2/N) * (y - ((m_current * x) + b_current))
@@ -28,7 +28,7 @@ def gradient_descent_runner(points, starting_b, starting_m, learning_rate, num_i
     b = starting_b
     m = starting_m
 
-    for i in xrange(num_iterations):
+    for i in range(num_iterations):
         b, m = step_gradient(b, m, array(points), learning_rate)
 
     return [b, m]
